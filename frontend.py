a3546ca600c5635f3ed536418ec6bb81c2d841a5
araul3927 <araulmaharjan@gmail.com>
2021-07-06 17:35:12 +0545

    api call added

--- frontend.py
+++ frontend.py
@@ -1,6 +1,5 @@
 import streamlit as st
-import requests
-import json
+from api_call import api_call
 
 import pandas as pd
 import numpy as np
@@ -11,28 +10,25 @@ import plotly.express as px
 def run():
     st.header("Check the emotion in the Text.")
 
-    option = st.selectbox(
-        "Algorithm you would like to use?", ("LogisticRegression", "Naive Bias")
+    selected_model = st.selectbox(
+        "Algorithm you would like to use?", ("Logistic Regression", "Naive Bias")
     )
-    input_feature = st.text_input(
-        "Enter the statment to predict the  emotion type", "I am happy to help you."
+    input_text = st.text_input(
+        "Enter the statment to predict the  emotion type", "Enter your text here"
     )
-
-    data = {"option": option, "input_feature": input_feature}
 
     if st.button("Predict"):
-        response = requests.post(" http://127.0.0.1:8000/predict", json=data)
-        loaded_json = json.loads(response.text)
+        response = api_call(selected_model, input_text)
 
-        emotion = np.array(loaded_json["emotion"])
-        data = np.array(loaded_json["data"])
+        emotion = np.array(response.get("emotion"))
+        data = np.array(response.get("data"))
 
         df = pd.DataFrame()
         df["Emotion"] = data
         df["emotion_propb"] = emotion.T
 
         st.markdown(
-            "The Emotion Predectied in the text is: " + loaded_json["prediction"]
+            f'The Emotion Predectied in the text is: { response.get("prediction") } '
         )
 
         fig = px.pie(df["Emotion"], values=df["emotion_propb"], names="Emotion")
